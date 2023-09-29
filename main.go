00a9b1d9fd29788c996e100c8870420f2dffe5be
star407 <starmatka786@gmail.com>
2023-09-30 00:33:21 +0530

    Added mongodb connection module and inserting new product to the mongoDB

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type product struct {
@@ -30,6 +36,8 @@ type user struct {
 	Updated_at string `json:"updated_at"`
 }
 
+var db *mongo.Database
+
 func main() {
 	//env initialization
 	err := godotenv.Load("local.env")
@@ -38,11 +46,17 @@ func main() {
 	}
 
 	port := os.Getenv("PORT")
+	uri := os.Getenv("MONGO_URI")
+
+	//mongodb connection
+	db, err = connectDb(uri)
+	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
 
 	//gin server router and handlers
 	router := gin.Default()
 	router.POST("/product", postProduct)
-
 	router.Run("localhost:" + port)
 }
 
@@ -54,5 +68,31 @@ func postProduct(c *gin.Context) {
 	}
 
 	//add new product to db
+	db.Collection("products").InsertOne(context.TODO(), newProduct)
 	c.IndentedJSON(http.StatusCreated, newProduct)
+}
+
+func connectDb(uri string) (*mongo.Database, error) {
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		return nil, err
+	}
+
+	// defer func() {
+	// 	fmt.Println("disconnecting")
+	// 	if err = client.Disconnect(context.TODO()); err != nil {
+	// 		return
+	// 	}
+	// }()
+
+	// Send a ping to confirm a successful connection
+	if err := client.Database("zock").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		return nil, err
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	db := client.Database("zock")
+	return db, nil
 }
