53a5d55104a6d42bfa24de2811299076a1f32581
star407 <starmatka786@gmail.com>
2023-10-01 16:49:14 +0530

    Added module to store local path of images stored in database in compressed_product_images key.

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -17,6 +17,7 @@ import (
 	"github.com/h2non/bimg"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 
 	"log"
@@ -47,10 +48,22 @@ func Consume(topic string) {
 		log.Fatal("ConsumePartition err: ", err)
 	}
 	defer partitionConsumer.Close()
+
+	//initializing mongo database client
+	db, err := initializeDbForConsumer()
+	if err != nil {
+		log.Fatal("Error occured while connecting to MongoDB: ", err)
+	}
+
+	//disconnect mongo-client socket after fetching product.
+	defer func() {
+		db.Client().Disconnect(context.TODO())
+	}()
+
 	for message := range partitionConsumer.Messages() {
 		log.Printf("product_id passed value: %s\n", string(message.Value))
 		intVar, _ := strconv.Atoi(string(message.Value))
-		prod := GetProduct(intVar)
+		prod := GetProduct(intVar, db)
 
 		//download every image of the product
 		for i, v := range prod.Product_images {
@@ -60,34 +73,47 @@ func Consume(topic string) {
 			if err != nil {
 				log.Fatal(err)
 			}
+			prod.Compressed_product_images = append(prod.Compressed_product_images, filePathName)
 		}
+		//store filePathName to product table in database
+		storFileLocalPath(prod.Product_id, prod.Compressed_product_images, db)
 	}
+
 }
 
-func GetProduct(product_id int) *product {
-
+func initializeDbForConsumer() (*mongo.Database, error) {
 	//initializing env variables for consumer
 	err := godotenv.Load("local.env")
 	if err != nil {
 		log.Fatalf("Some error occured. Err: %s", err)
-		return nil
+		return nil, err
 	}
 
 	//connect db for consume
 	db, err := database.ConnectDb(os.Getenv("MONGO_URI"))
 	if err != nil {
 		log.Fatal("db connection err: ", err)
-		return nil
+		return nil, err
 	}
+	return db, nil
+}
 
-	//disconnect mongo-client socket after fetching product.
-	defer func() {
-		db.Client().Disconnect(context.TODO())
-	}()
+// module to update local paths in database
+func storFileLocalPath(product_id int, paths []string, db *mongo.Database) {
+	filter := bson.D{primitive.E{Key: "product_id", Value: product_id}}
+
+	update := bson.D{primitive.E{Key: "$set", Value: bson.D{
+		primitive.E{Key: "compressed_product_images", Value: paths},
+	}}}
+
+	db.Collection("products").FindOneAndUpdate(context.TODO(), filter, update)
+}
+
+func GetProduct(product_id int, db *mongo.Database) *product {
 
 	//get docuemnt using product_id
 	var responseProduct product
-	err = db.Collection("products").FindOne(context.TODO(), bson.D{{"product_id", product_id}}).Decode(&responseProduct)
+	err := db.Collection("products").FindOne(context.TODO(), bson.D{{"product_id", product_id}}).Decode(&responseProduct)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
 			// This error means your query did not match any documents.
@@ -117,6 +143,7 @@ func downloadFile(URL, filePath string) error {
 		return err
 	}
 	defer file.Close()
+
 	//compress image
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
